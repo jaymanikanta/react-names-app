6074f62c55e19ad6686725a7e60c484ae86a30c2
Manikanta Jayanth <jayanth_manikanta@yahoo.com>
2018-12-22 16:31:05 -0600

    V2.0.0, includes adding major functionality like shortlisting names and filtering names based on search.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,46 @@ import React, { Component } from "react";
 import NamesList from "./components/NamesList";
 import Credit from "./components/Credit";
 import Search from "./components/Search";
+import ShortList from "./components/ShortList";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: "",
+      favourites: []
+    };
+  }
+
+  filterUpdate(value) {
+    this.setState({ filterText: value });
+  }
+
+  addFavourite(id) {
+    const favList = this.state.favourites.concat([id]);
+    this.setState({ favourites: favList });
+  }
+
   render() {
+    console.log("From parent component", this.state.filterText);
     return (
       <div>
-        <Search />
-        <NamesList data={this.props.data} />
-        <Credit />
+        <Search
+          filterText={this.state.filterText}
+          filterUpdate={this.filterUpdate.bind(this)}
+        />
+        <main>
+          <ShortList
+            favourites={this.state.favourites}
+            data={this.props.data}
+          />
+          <NamesList
+            data={this.props.data}
+            filterText={this.state.filterText}
+            addFavourite={this.addFavourite.bind(this)}
+          />
+          <Credit />
+        </main>
       </div>
     );
   }
--- src/components/NamesList.js
+++ src/components/NamesList.js
@@ -2,16 +2,28 @@ import React, { Component } from "react";
 
 class NamesList extends Component {
   render() {
-    const { data } = this.props;
-    const namesList = data.map(name => {
-      return (
-        <li key={name.id} className={name.sex}>
-          {name.name}
-        </li>
-      );
-    });
+    const { data, filterText, addFavourite } = this.props;
+    const namesList = data
+      .filter(name => {
+        return name.name.toLowerCase().indexOf(filterText.toLowerCase()) >= 0;
+      })
+      .map(name => {
+        return (
+          <li
+            key={name.id}
+            className={name.sex}
+            onClick={() => addFavourite(name.id)}
+          >
+            {name.name}
+          </li>
+        );
+      });
 
-    return <ul>{namesList}</ul>;
+    return (
+      <div>
+        <ul>{namesList}</ul>
+      </div>
+    );
   }
 }
 
